afa31be6890fa7cedbdbc7fe9b1c5c4711965188
uekyorf <uekyorf@gmail.com>
2019-09-02 18:21:31 +0900

    Add: ディスプレイの取得を追加

--- display/display.go
+++ display/display.go
@@ -2,7 +2,45 @@ package display
 
 import (
 	"fmt"
+	"log"
+	"os/exec"
+	"regexp"
+	"strings"
 )
+
+func getDisplay() []display {
+	out, err := exec.Command("xrandr").Output()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	text := strings.Split(string(out), "\n")
+
+	dps := []display{}
+	for i := 0; i < len(text); i++ {
+		rep := regexp.MustCompile(`connected`)
+		if rep.MatchString(text[i]) {
+			dp := display{}
+
+			dp = getDisplayInfo(text[i])
+			sizes := []string{}
+
+			for j := i + 1; j < len(text); j++ {
+				rep = regexp.MustCompile(`\s*\d+x\d+i?\s`)
+				if rep.MatchString(text[j]) {
+					size := getDisplaySize(text[j])
+					sizes = append(sizes, size)
+					continue
+				}
+				i = j - 1
+				break
+			}
+			dp.sizes = sizes
+			dps = append(dps, dp)
+		}
+	}
+	return dps
+}
 
 func Status() {
 	displays := getDisplay()
